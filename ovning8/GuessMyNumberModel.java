0a547456d1eb0bb28f97a2a2009d0d035b318dd9
Philip <philipax@kth.se>
2022-05-30 14:48:10 +0200

    Inline changes

--- ovning8/GuessMyNumberModel.java
+++ ovning8/GuessMyNumberModel.java
@@ -1,4 +1,60 @@
 package ovning8;
+import java.util.Random;
 
 public class GuessMyNumberModel {
+    private int secretNr;
+    private int guess;
+    private int nrOfGuesses;
+    private final int limit;
+
+    public GuessMyNumberModel(int limit){
+        this.limit = limit;
+    }
+
+    public void initGame(){
+        nrOfGuesses = 0;
+        Random rand = new Random();
+        this.secretNr = rand.nextInt(this.limit);
+    }
+    public int compareGuess(){
+        if (guess > secretNr){
+            return 1;
+        }
+        else if (guess == secretNr){
+            return 0;
+        }
+        else return -1;
+    }
+
+    public String compareGuessToStr(){
+        int nr = compareGuess();
+        if (nr == 1){
+            return "to high";
+        }
+        else if (nr == -1){
+            return "to low";
+        }
+        else return "correct!";
+    }
+
+    public int getGuess() {
+        return guess;
+    }
+
+    public void setGuess(int guess) {
+        this.guess = guess;
+        nrOfGuesses++;
+    }
+
+    public int getMAX(){
+        return this.limit;
+    }
+
+    public int getNoOfGuesses(){
+        return nrOfGuesses;
+    }
+
+    public int getSecret(){
+        return secretNr;
+    }
 }
--- ovning8/Main.java
+++ ovning8/Main.java
@@ -1,10 +1,35 @@
 package ovning8;
 
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        GuessMyNumberModel guessModel = new GuessMyNumberModel(100);
+        Scanner scan = new Scanner(System.in);
+        String cont = "y";
+        while (cont.toLowerCase().charAt(0) == 'y') {
+            guessModel.initGame();
+            int guess;
+            do {
+                System.out.print(
+                        "Make a guess [1.." + guessModel.getMAX() + "]: ");
+                String numStr = scan.nextLine().trim();
+                guess = Integer.parseInt(numStr);
+                guessModel.setGuess(guess);
+                System.out.println("Your guess is " +
+                        guessModel.compareGuessToStr());
+                System.out.println("Number of guesses: " +
+                        guessModel.getNoOfGuesses());
+            }
+            while (guess != guessModel.getSecret());
+            System.out.println("Congratulations!");
+            System.out.print("Continue (y/n)? ");
+            cont = scan.nextLine();
+        }
+        System.out.println("Bye bye.");
+        scan.close();
 
     }
 }
